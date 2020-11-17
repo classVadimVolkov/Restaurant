a223037fdc311633ec87fba2af5f8fa5303c0e55
Vadim Volkov <antibiotik15@ya.ru>
2020-11-17 15:51:13 +0300

    without advertisement

---
+++ ConsoleHelper.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task27.task2712;
+
+import com.javarush.task.task27.task2712.kitchen.Dish;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    // для вывода message в консоль
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    //для чтения строки с консоли
+    public static String readString() throws IOException {
+        return bufferedReader.readLine();
+    }
+
+    // просит пользователя выбрать блюдо и добавляет его в список
+    public static List<Dish> getAllDishesForOrder() throws IOException {
+        ConsoleHelper.writeMessage(Dish.allDishesToString());
+        ConsoleHelper.writeMessage("Please, choose dishes and then type 'exit':");
+        List<Dish> listOfDishesForOrder = new ArrayList<>();
+
+        while (true) {
+            String dishName = ConsoleHelper.readString().trim();
+            if (dishName.equals("exit")) {
+                break;
+            }
+            try {
+                Dish dish = Dish.valueOf(dishName);
+                listOfDishesForOrder.add(dish);
+                writeMessage(dishName + " has been successfully added to your order");
+            } catch (Exception e) {
+                writeMessage(dishName + " hasn't been detected");
+            }
+        }
+
+
+        return listOfDishesForOrder;
+    }
+
+}
---
+++ kitchen/Order.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import com.javarush.task.task27.task2712.ConsoleHelper;
+import com.javarush.task.task27.task2712.Tablet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Order {
+    private final Tablet tablet;
+    protected List<Dish> dishes;
+
+    public Order(Tablet tablet) throws IOException {
+        this.tablet = tablet;
+        this.dishes = ConsoleHelper.getAllDishesForOrder();
+        ConsoleHelper.writeMessage(this.toString());
+    }
+
+    public int getTotalCookingTime() {
+        return dishes.stream().mapToInt(Dish::getDuration).sum();
+    }
+
+    public boolean isEmpty() {
+        return dishes.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        if (dishes.isEmpty()) {
+            return "";
+        } else {
+            return "Your order: " + Arrays.toString(dishes.toArray()) + " of " + tablet;
+        }
+    }
+}
